5cba09c6bcc2ec313117da628d80d1eea6ecb46b
nab138 <NicholasDanielSharp@gmail.com>
2021-06-24 12:56:21 -0400

    Added .gitignore

--- publish.sh
+++ publish.sh
@@ -5,6 +5,7 @@ dpkg-scanpackages debs/ > Packages
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
 git add .
+echo "What commit message would you like to use?"
 read commitcomment
 git commit -a -m "$commitcomment"
 git push
